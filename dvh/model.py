482ef370610c9eba1bb173fb312e0aa3eb0a97da
mart2010 <mart2010.l@gmail.com>
2017-09-01 23:03:52 +0200

    ongoing
    Signed-off-by: mart2010 <mart2010.l@gmail.com>

--- dvh/model.py
+++ dvh/model.py
@@ -1,4 +1,21 @@
-import yaml
+import ruamel.yaml
+
+
+# class Column(yaml.YAMLObject):
+#     yaml_tag = '!Column'
+#
+#     def __init__(self, type, size=None):
+#         # self.name = validate_name(name, 'Column')
+#         self.type = type
+#         self.size = size
+#
+#     def __repr__(self):
+#         return '%s(type=%r, size=%r)' % (self.__class__.__name__, self.type, self.size)
+#
+#     def __eq__(self, other):
+#         if not isinstance(other, Column):
+#             return False
+#         return self.type == other.size and self.size == other.size
 
 
 def validate_name(col_or_table, vtype="Table"):
@@ -18,8 +35,33 @@ def validate_name(col_or_table, vtype="Table"):
 # mettre les elements (qui ont valeurs par defaut) explicites de le code en relation avec les var dans les mapping default
 
 
-class DVModel(yaml.YAMLObject):
-    yaml_tag = '!DVModel'
+class Base(object):
+
+    def validate(self):
+        raise NotImplementedError
+
+    # yaml.load() is not using __init__, so default attribute are generated dynamically
+    def __getattr__(self, name):
+        # otherwise yaml fails while accessing: data.__setstate__(state)
+        if name[:2] != '__':
+            default = None
+            setattr(self, name, default)
+            return default
+        else:
+            raise AttributeError("'{0}' object has no attribute '{1}'".format(self.__class__.__name__, name))
+
+    def __repr__(self):
+        atts = ", ".join(["{0}={1}".format(k, repr(v)) for k, v in self.__dict__.items()])
+        return "{0}({1})".format(self.__class__.__name__, atts)
+
+    def __ne__(self, other):
+        return not self.__eq__(self, other)
+
+    def __str__(self):
+        return self.__repr__()
+
+
+class DVModel(Base):
 
     def __init__(self, tables_dic):
         self.tables = tables_dic
@@ -27,7 +69,7 @@ class DVModel(yaml.YAMLObject):
 
     def names_creation_order(self):
         names_ordered = []
-        for k, _ in sorted(self.tables.items(), key=lambda kv: str(kv[1].sort_order) + kv[0]):
+        for k, _ in sorted(self.tables.items(), key=lambda kv: kv[1].sort_order + kv[0]):
             # print("ddl order key:" + k)
             names_ordered.append(k)
         return names_ordered
@@ -39,54 +81,47 @@ class DVModel(yaml.YAMLObject):
         for n in self.names_ddl_order:
             print("I will create DDL for: " + n)
 
-    def __repr__(self):
-        return '%s(tables=%r)' % (self.__class__.__name__, self.tables)
+    # def __repr__(self):
+    #     return '%s(tables=%r)' % (self.__class__.__name__, self.tables)
 
-
-
-class Column(yaml.YAMLObject):
-    yaml_tag = '!Column'
-
-    def __init__(self, type, size=None):
-        # self.name = validate_name(name, 'Column')
-        self.type = type
-        self.size = size
-
-    def __repr__(self):
-        return '%s(type=%r, size=%r)' % (self.__class__.__name__, self.type, self.size)
-
-
-class Base(yaml.YAMLObject):
-
-    # probably a clever way to implement at this level..
-    def ddl_expression(self):
-        return "ddl " + self._repr__()
+    def __eq__(self, other):
+        if not isinstance(other, DVModel):
+            return False
+        return self.tables == other.tables
 
 
 class Hub(Base):
-    yaml_tag = '!Hub'
-    sort_order = 1
+    sort_order = '1'
 
-    def __init__(self, keys, surrogate_key=None):
-        super().__init__()
-        if len(keys) < 1:
-            raise ValueError("Hub must have at least one natural key")
-        self.keys = keys
-        if surrogate_key:
-            if surrogate_key.get('name') is None or surrogate_key.get('sequence') is None:
-                raise ValueError("Surrogate definition requires both a name and a sequence")
-        self.surrogate_key = surrogate_key
+    def validate(self):
+        if self.nat_keys is None or len(self.nat_keys) == 0:
+            raise ValueError("Hub must have at least one 'nat_keys'")
+
+        if self.sur_key is None:
+            if len(self.nat_keys) > 1:
+                raise ValueError("Hub without 'sur_key' must have a unique 'nat_keys'")
+        elif len(self.sur_key.values()) > 1:
+            raise ValueError("Only one 'sur_key' definition is possible")
+        else:
+            sk_def = list(self.sur_key.values())
+            if sk_def[0].get('sequence') is None:
+                raise ValueError("Hub's 'sur_key' requires a 'sequence' definition")
+
 
     def get_primarey_key(self):
         pass
 
-    def __repr__(self):
-        return '%s(keys=%r, surrogate_key=%r)' % (self.__class__.__name__, self.keys, self.surrogate_key)
+    #def __repr__(self):
+    #    return '%s(keys=%r, surrogate_key=%r)' % (self.__class__.__name__, self.keys, self.surrogate_key)
+
+    def __eq__(self, other):
+        if not isinstance(other, Hub):
+            return False
+        return self.keys == other.keys and self.surrogate_key == other.surrogate_key
 
 
 class Sat(Base):
-    yaml_tag = '!Sat'
-    sort_order = 2
+    sort_order = '2'
 
     def __init__(self, atts, hub):
         super().__init__()
@@ -95,13 +130,17 @@ class Sat(Base):
         self.atts = atts
         self.hub = hub
 
-    def __repr__(self):
-        return '%s(atts=%r, hub=%r)' % (self.__class__.__name__, self.atts, self.hub)
+    #def __repr__(self):
+    #    return '%s(atts=%r, hub=%r)' % (self.__class__.__name__, self.atts, self.hub)
+
+    def __eq__(self, other):
+        if not isinstance(other, Sat):
+            return False
+        return self.atts == other.atts and self.hub == other.hub
 
 
 class Link(Base):
-    yaml_tag = '!Link'
-    sort_order = 3
+    sort_order = '3'
 
     def __init__(self, hubs):
         super().__init__()
@@ -109,13 +148,17 @@ class Link(Base):
             raise ValueError("Link '{0}' must refer to two or more Hubs".format(self.name))
         self.hubs = hubs
 
-    def __repr__(self):
-        return '%s(hubs=%r)' % (self.__class__.__name__, self.hubs)
+    #def __repr__(self):
+    #    return '%s(hubs=%r)' % (self.__class__.__name__, self.hubs)
+
+    def __eq__(self, other):
+        if not isinstance(other, Link):
+            return False
+        return self.hubs == other.hubs
 
 
 class SatLink(Base):
-    yaml_tag = '!SatLink'
-    sort_order = 4
+    sort_order = '4'
 
     def __init__(self, atts, link):
         super().__init__()
@@ -124,22 +167,23 @@ class SatLink(Base):
         self.atts = atts
         self.link = link
 
-    def __repr__(self):
-        return '%s(atts=%r, link=%r)' % (self.__class__.__name__, self.atts, self.link)
+    #def __repr__(self):
+    #    return '%s(atts=%r, link=%r)' % (self.__class__.__name__, self.atts, self.link)
+
+    def __eq__(self, other):
+        if not isinstance(other, SatLink):
+            return False
+        return self.atts == other.atts and self.link == other.link
 
 
+yaml = ruamel.yaml.YAML()
+yaml.register_class(DVModel)
+yaml.register_class(Hub)
+yaml.register_class(Sat)
+yaml.register_class(Link)
+yaml.register_class(SatLink)
 
-h1 = Hub({'id': {'type': 'number', 'size': '(3,2)'}})
-h2 = Hub({'id': {'type': 'number', 'size': '(9)'}})
-s11 = Sat({'att1': {'type': 'number', 'size': '(3,2)'}, 'att2': {'type': 'varchar', 'size': '(15)'}}, h1)
-s12 = Sat({'att1': {'type': 'number', 'size': '(10)'}, 'att2': {'type': 'varchar', 'size': '(15)'}}, h1)
-s21 = Sat({'attx': {'type': 'number', 'size': '(10)'}, 'atty': {'type': 'varchar', 'size': '(15)'}}, h2)
+# yaml_tag approach does work for default optional param
+# https://stackoverflow.com/questions/7224033/default-constructor-parameters-in-pyyaml
+# so revert to using own constructor impl
 
-l = Link([h1, h2])
-sl1 = SatLink({'attt': {'type': 'date'}}, l)
-
-t_d = DVModel({'sl1': sl1, 's21': s21, 'h2': h2, 'h1': h1, 's11': s11, 's12': s12, 'l': l})
-
-print(yaml.dump(t_d))
-
-print(t_d.generate_ddl(with_drop=False))
